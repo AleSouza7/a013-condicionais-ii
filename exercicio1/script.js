3f799556297c24a7c901c36a9e0e6863e825d080
Alesouza7 <alexcostasouza86@yahoo.com>
2022-08-09 18:12:26 -0300

    Atualização

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,13 @@
-const num = //seu numero aqui
+const num = Number(prompt("O numero é divisil por 2 ou por 3?")) //seu numero aqui
 
+if(num %2 === 0){
+    console.log("E divisil por 2");
+    if(num %3===0){
+        console.log("E divisil por 3")
+    }
+}
+
+//
+if(num %2 === 0 || num %3 === 0){
+    console.log("E divisil por 2 ou por 3")
+}
